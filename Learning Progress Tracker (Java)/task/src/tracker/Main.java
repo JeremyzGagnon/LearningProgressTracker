a55a43a67fc666b8b6daa386002d7a2818efec9b
Jeremy Gagnon <jeremyzgagnon@gmail.com>
2023-05-11 18:21:04 -0300

    first stage

--- Learning Progress Tracker (Java)/task/src/tracker/Main.java
+++ Learning Progress Tracker (Java)/task/src/tracker/Main.java
@@ -1,8 +1,21 @@
 package tracker;
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Learning Progress Tracker");
+        String usersInput = scanner.nextLine();
 
-        System.out.println("Hello, World!");
+        while (!usersInput.equals("exit")) {
+            if (usersInput.isEmpty() || usersInput.isBlank()) {
+                System.out.println("No input.");
+            } else {
+                System.out.println("Unknown command!");
+            }
+            usersInput = scanner.nextLine();
+        }
+        System.out.println("Bye!");
+
+
     }
 }
